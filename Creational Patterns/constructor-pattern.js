56cf43a7c78b225e1b40e958b27664981133971d
Bhagawat Mishra <bmishra@ingenuitygaming.com>
2024-06-10 18:36:27 +0530

    Constructor Pattern;

---
+++ Creational Patterns/constructor-pattern.js
@@ -0,0 +1,20 @@
+function Human(name, age, occupation) { //ES5 function based constructor 
+    //defining properties inside the constructor function
+    //constructor initializing the property values upon object creation
+    this.name = name;
+    this.age = age;
+    this.occupation = occupation;
+    //defining a method inside the constructor function
+    this.describe = function () {
+        console.log(`${this.name} is a ${this.age}-year-old ${this.occupation}`);
+    }
+}
+
+//creating a "person" object using the Human constructor
+//the constructor uses the arguments passed into it to
+//initialize the property values name, age and sex
+var person = new Human("Elle", "23", "Engineer");
+person.describe();
+//creating a second object
+var newperson = new Human("Joe", "13", "Painter");
+newperson.describe();
